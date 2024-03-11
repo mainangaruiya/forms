0499e75a93d903fd19fd0284565c46c1b9f946d9
Wizardwa <ewaatdavis@gmail.com>
2024-03-11 20:49:50 +0300

    Success alert

--- info.php
+++ info.php
@@ -15,10 +15,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 		$query = "insert into users(first_name,last_name,gender,county,hobbies,description) values('$firstName','$lastName','$gender','$county','$hobbies', '$desc')";
 		$result = mysqli_query($conn,$query);
 		
-		if (mysql_error($result)){
-			echo "An error was encounterd while inserting details to the database";
-		}else{
-			echo "Successful";
-		}
+		echo '<script>alert("Succesful")</script>';
 	}
 }
